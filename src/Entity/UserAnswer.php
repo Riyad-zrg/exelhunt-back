7153fd94218ba40e43e7fcf3819387e5a1179d94
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-10-01 11:50:09 +0200

    feat: Generation of UserAnswer Entity

---
+++ src/Entity/UserAnswer.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserAnswerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserAnswerRepository::class)]
+class UserAnswer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private array $content = [];
+
+    #[ORM\Column]
+    private ?bool $isCorrect = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $sendAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): array
+    {
+        return $this->content;
+    }
+
+    public function setContent(array $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function isCorrect(): ?bool
+    {
+        return $this->isCorrect;
+    }
+
+    public function setIsCorrect(bool $isCorrect): static
+    {
+        $this->isCorrect = $isCorrect;
+
+        return $this;
+    }
+
+    public function getSendAt(): ?\DateTimeImmutable
+    {
+        return $this->sendAt;
+    }
+
+    public function setSendAt(\DateTimeImmutable $sendAt): static
+    {
+        $this->sendAt = $sendAt;
+
+        return $this;
+    }
+}
