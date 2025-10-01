b90afb558fd1f00af71b7f39c842b5fef267676a
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-10-01 12:03:00 +0200

    feat: Generation of Code Entity

---
+++ src/Entity/Code.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CodeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CodeRepository::class)]
+class Code
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $code = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $expireAt = null;
+
+    #[ORM\OneToOne(inversedBy: 'code')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Hunt $Hunt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getExpireAt(): ?\DateTimeImmutable
+    {
+        return $this->expireAt;
+    }
+
+    public function setExpireAt(\DateTimeImmutable $expireAt): static
+    {
+        $this->expireAt = $expireAt;
+
+        return $this;
+    }
+
+    public function getHunt(): ?Hunt
+    {
+        return $this->Hunt;
+    }
+
+    public function setHunt(?Hunt $Hunt): static
+    {
+        $this->Hunt = $Hunt;
+
+        return $this;
+    }
+}
