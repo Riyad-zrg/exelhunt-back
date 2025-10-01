58f5a24e965ba04e05fc242f360d58c2f2378473
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-10-01 11:38:05 +0200

    feat: Generation of PuzzleAnswer Entity

---
+++ src/Entity/PuzzleAnswer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PuzzleAnswerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PuzzleAnswerRepository::class)]
+class PuzzleAnswer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 15)]
+    private ?string $type = null;
+
+    #[ORM\Column]
+    private array $content = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getContent(): array
+    {
+        return $this->content;
+    }
+
+    public function setContent(array $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
