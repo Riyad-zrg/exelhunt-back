356f849da344d5edf1914a4861ebfb974faca803
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-10-03 10:59:58 +0200

    feat: Creation of dummy data for Puzzle Entity

--- src/Factory/PuzzleFactory.php
+++ src/Factory/PuzzleFactory.php
@@ -31,21 +31,28 @@ final class PuzzleFactory extends PersistentProxyObjectFactory
      */
     protected function defaults(): array|callable
     {
+        $faker = self::faker();
+
         return [
-            'content' => self::faker()->text(500),
-            'hunt' => HuntFactory::new(),
-            'index' => self::faker()->randomNumber(),
-            'title' => self::faker()->text(100),
+            'title' => $faker->sentence(5, true),
+            'content' => $faker->paragraph(3, true),
+            'hint' => $faker->boolean(70) ? $faker->sentence(8) : null,
+            'index' => 0,
+            'timeLimit' => $faker->boolean(50) ? (new \DateTime())->setTime(0, $faker->numberBetween(5, 60)) : null,
+            'malus' => $faker->boolean(40) ? (new \DateTime())->setTime(0, $faker->numberBetween(1, 15)) : null,
+            'hunt' => HuntFactory::random(),
         ];
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-     */
     protected function initialize(): static
     {
         return $this
-            // ->afterInstantiate(function(Puzzle $puzzle): void {})
-        ;
+            ->afterInstantiate(function (Puzzle $puzzle): void {
+                $hunt = $puzzle->getHunt();
+                if ($hunt) {
+                    $index = count($hunt->getPuzzles()) + 1;
+                    $puzzle->setIndex($index);
+                }
+            });
     }
 }
