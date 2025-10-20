ed58adfb1af7ecdf48097439e89f468cf9dda2a5
Adrien Cadalen <adrien.cadalen@etudiant.univ-reims.fr>
2025-10-20 16:33:44 +0200

    fix: Modified testing routes on CreationController to be clearer and easier

--- src/Controller/CreationController.php
+++ src/Controller/CreationController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class CreationController extends AbstractController
 {
-    #[Route('/creation', name: 'app_creation')]
+    #[Route('/create', name: 'app_creation')]
     public function index(): Response
     {
         return $this->render('creation/index.html.twig', [
@@ -16,15 +16,39 @@ final class CreationController extends AbstractController
         ]);
     }
 
-    #[Route('/creation/hunt', name: 'app_creation_hunt')]
-    public function hunt(): Response
+    #[Route('/create/text_puzzle', name: 'app_creation_txt_puzzle')]
+    public function text_puzzle(): Response
     {
-        return $this->render('creation/hunt.html.twig', [
+        return $this->render('creation/puzzle/txt.html.twig', [
             'controller_name' => 'CreationController',
         ]);
     }
 
-    #[Route('/creation/team', name: 'app_creation_team')]
+    #[Route('/create/mcq_puzzle', name: 'app_creation_mcq_puzzle')]
+    public function qcm_puzzle(): Response
+    {
+        return $this->render('creation/puzzle/mcq.html.twig', [
+            'controller_name' => 'CreationController',
+        ]);
+    }
+
+    #[Route('/create/gps_puzzle', name: 'app_creation_gps_puzzle')]
+    public function gps_puzzle(): Response
+    {
+        return $this->render('creation/puzzle/gps.html.twig', [
+            'controller_name' => 'CreationController',
+        ]);
+    }
+
+    #[Route('/create/qrc_puzzle', name: 'app_creation_qrc_puzzle')]
+    public function qrc_puzzle(): Response
+    {
+        return $this->render('creation/puzzle/qrc.html.twig', [
+            'controller_name' => 'CreationController',
+        ]);
+    }
+
+    #[Route('/create/team', name: 'app_creation_team')]
     public function team(): Response
     {
         return $this->render('creation/team.html.twig', [
