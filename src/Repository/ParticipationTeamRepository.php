7a26a4da215d9e6e2c6737c0b71bef3c302d3112
Nicolas Desban <nicolas.desban@etudiant.univ-reims.fr>
2025-10-20 16:59:50 +0200

    feat: Add ParticipationTeamRepository for managing ParticipationTeam entities

--- src/Repository/ParticipationTeamRepository.php
+++ src/Repository/ParticipationTeamRepository.php
@@ -16,28 +16,28 @@ class ParticipationTeamRepository extends ServiceEntityRepository
         parent::__construct($registry, ParticipationTeam::class);
     }
 
-    //    /**
-    //     * @return ParticipationTeam[] Returns an array of ParticipationTeam objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+//    /**
+//     * @return ParticipationTeam[] Returns an array of ParticipationTeam objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
 
-    //    public function findOneBySomeField($value): ?ParticipationTeam
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+//    public function findOneBySomeField($value): ?ParticipationTeam
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
 }
