6e69561236130aa36c08c71a58beb4efa99d11bf
Adrien Cadalen <adrien.cadalen@etudiant.univ-reims.fr>
2025-10-21 16:55:35 +0200

    feat: Enhanced QR code puzzle creation with download functionality and random QR codes

--- src/Controller/CreationController.php
+++ src/Controller/CreationController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
-use Endroid\QrCode\Label\Font\OpenSans;
-use Endroid\QrCode\Label\LabelAlignment;
 use Endroid\QrCode\RoundBlockSizeMode;
 use Endroid\QrCode\Writer\PngWriter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -39,14 +37,16 @@ final class CreationController extends AbstractController
         return $this->render('creation/puzzle/gps.html.twig');
     }
 
-    #[Route('/create/qrc_puzzle', name: 'app_creation_qrc_puzzle')]
+    #[Route('/create/qrc_puzzle/', name: 'app_creation_qrc_puzzle')]
     public function qrc_puzzle(): Response
     {
+        $randomString = bin2hex(random_bytes(16));
+
         $builder = new Builder(
             writer: new PngWriter(),
             writerOptions: [],
             validateResult: false,
-            data: 'test again',
+            data: $randomString,
             encoding: new Encoding('UTF-8'),
             errorCorrectionLevel: ErrorCorrectionLevel::High,
             size: 300,
@@ -57,8 +57,33 @@ final class CreationController extends AbstractController
         $result = $builder->build();
 
         return $this->render('creation/puzzle/qrc.html.twig', [
-            'qrc' => $result->getDataUri(),
+            'qrc' => $result,
+            'randomString' => $randomString,
         ]);
+    }
+
+    #[Route('/create/qrc_puzzle/download/{randomString}', name: 'app_creation_qrc_dl')]
+    public function qrc_download(string $randomString): Response
+    {
+        $builder = new Builder(
+            writer: new PngWriter(),
+            writerOptions: [],
+            validateResult: false,
+            data: $randomString,
+            encoding: new Encoding('UTF-8'),
+            errorCorrectionLevel: ErrorCorrectionLevel::High,
+            size: 300,
+            margin: 10,
+            roundBlockSizeMode: RoundBlockSizeMode::Margin,
+        );
+
+        $result = $builder->build();
+
+        $response = new Response($result->getString());
+        $response->headers->set('Content-Type', 'image/png');
+        $response->headers->set('Content-Disposition', 'attachment; filename="qrcode.png"');
+
+        return $response;
     }
 
     #[Route('/create/team', name: 'app_creation_team')]
