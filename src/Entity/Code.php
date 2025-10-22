96f690bfaa0b10475ef840ea95300514d43df95e
Nicolas Desban <nicolas.desban@etudiant.univ-reims.fr>
2025-10-22 16:45:30 +0200

    fix: Change code property type from int to string and update related methods

--- src/Entity/Code.php
+++ src/Entity/Code.php
@@ -24,7 +24,7 @@ class Code
 
     #[ORM\OneToOne(inversedBy: 'code')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?hunt $hunt = null;
+    private ?Hunt $hunt = null;
 
     #[ORM\OneToOne(mappedBy: 'code', cascade: ['persist', 'remove'])]
     private ?TeamPlayer $teamPlayer = null;
@@ -70,12 +70,12 @@ class Code
         return $this;
     }
 
-    public function getHunt(): ?hunt
+    public function getHunt(): ?Hunt
     {
         return $this->hunt;
     }
 
-    public function setHunt(?hunt $hunt): static
+    public function setHunt(?Hunt $hunt): static
     {
         $this->hunt = $hunt;
 
@@ -90,12 +90,12 @@ class Code
     public function setTeamPlayer(?TeamPlayer $teamPlayer): static
     {
         // unset the owning side of the relation if necessary
-        if ($teamPlayer === null && $this->teamPlayer !== null) {
+        if (null === $teamPlayer && null !== $this->teamPlayer) {
             $this->teamPlayer->setCode(null);
         }
 
         // set the owning side of the relation if necessary
-        if ($teamPlayer !== null && $teamPlayer->getCode() !== $this) {
+        if (null !== $teamPlayer && $teamPlayer->getCode() !== $this) {
             $teamPlayer->setCode($this);
         }
 
