2ec3d1ecbbdcaa730582627cc07d59717d195759
Nicolas Desban <nicolas.desban@etudiant.univ-reims.fr>
2025-10-24 14:46:02 +0200

    feat: Add TeamPlayerFixtures for generating TeamPlayer entities linked to Hunts

---
+++ src/DataFixtures/TeamPlayerFixtures.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\CodeFactory;
+use App\Factory\HuntFactory;
+use App\Factory\TeamPlayerFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Random\RandomException;
+
+class TeamPlayerFixtures extends Fixture
+{
+    /**
+     * @throws RandomException
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $hunts = HuntFactory::createMany(50, function () {
+            $isPlayable = 1 === random_int(0, 1);
+
+            return [
+                'isTeamPlayable' => $isPlayable,
+                'teamPlayerMax' => $isPlayable ? random_int(2, 8) : null,
+            ];
+        });
+
+        foreach ($hunts as $hunt) {
+            if (!$hunt->isTeamPlayable()) {
+                continue;
+            }
+
+            $max = $hunt->getTeamPlayerMax() ?? 5;
+            $count = random_int(1, min(5, max(1, $max)));
+
+            TeamPlayerFactory::createMany($count, function () use ($hunt) {
+                $isPublic = (bool) random_int(0, 1);
+
+                $overrides = [
+                    'hunt' => $hunt,
+                    'isPublic' => $isPublic,
+                    'nbPlayers' => random_int(1, max(1, $hunt->getNbPlayers() ?? 4)),
+                ];
+
+                if (!$isPublic && class_exists(CodeFactory::class)) {
+                    $overrides['code'] = CodeFactory::new();
+                }
+
+                return $overrides;
+            });
+        }
+
+        $manager->flush();
+    }
+}
