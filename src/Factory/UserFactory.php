a53e300e7ff67fdd1ca621edef0b816dc065fcf2
Nicolas Desban <nicolas.desban@etudiant.univ-reims.fr>
2025-10-24 16:25:15 +0200

    feat: Enhance UserFactory to improve avatar generation and role handling

--- src/Factory/UserFactory.php
+++ src/Factory/UserFactory.php
@@ -33,47 +33,74 @@ final class UserFactory extends PersistentProxyObjectFactory
     protected function defaults(): array|callable
     {
         $faker = Factory::create('fr_FR');
-        $primaryRole = $faker->randomElement(['GUEST', 'ADMIN', 'STAFF', 'PLAYER', 'USER']);
 
-        // Déterminer les rôles effectifs selon la règle
+        $primaryRole = $faker->randomElement(['GUEST', 'ADMIN', 'STAFF', 'PLAYER', 'CONCEPTOR']);
+
         $roles = match ($primaryRole) {
             'GUEST' => ['GUEST'],
             'ADMIN' => ['ADMIN', 'STAFF', 'PLAYER', 'CONCEPTOR', 'USER'],
             'STAFF' => ['STAFF', 'PLAYER', 'CONCEPTOR', 'USER'],
             'CONCEPTOR' => ['CONCEPTOR', 'PLAYER', 'USER'],
             'PLAYER' => ['PLAYER', 'USER'],
-            'USER' => ['USER'],
+            default => ['USER'],
         };
 
+        $defaultPath = __DIR__.'/../DataFixtures/img/userDefault.jpg';
+        if (is_readable($defaultPath)) {
+            $avatarData = base64_encode(file_get_contents($defaultPath));
+        } else {
+            $color = $faker->hexColor();
+            $initial = strtoupper(substr($faker->firstName(), 0, 1));
+            $svg = sprintf(
+                '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200"><rect width="100%%" height="100%%" fill="%s"/><text x="50%%" y="50%%" font-size="80" fill="#ffffff" text-anchor="middle" dominant-baseline="central">%s</text></svg>',
+                $color,
+                htmlspecialchars($initial, ENT_QUOTES)
+            );
+            $avatarData = base64_encode($svg);
+        }
+
+        $hasAddress = $faker->boolean(60) ? AddressFactory::new() : null;
+
         $defaults = [
-            'avatar' => base64_encode(file_get_contents(__DIR__.'/../DataFixtures/img/userDefault.jpg')),
+            'avatar' => $avatarData,
             'createdAt' => \DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-500 days', 'now')),
-            'nickname' => $faker->userName(),
-            'password' => $faker->password(30),
+            'nickname' => $faker->unique()->userName(),
+            'password' => $faker->password(12),
             'roles' => $roles,
+            'address' => $hasAddress,
         ];
 
-        // Remplir les champs optionnels si ce n'est pas un GUEST
         if ('GUEST' !== $primaryRole) {
             $defaults = array_merge($defaults, [
                 'firstname' => $faker->firstName(),
                 'lastname' => $faker->lastName(),
-                'email' => $faker->email(),
-                'biography' => $faker->paragraph(),
-                'Address' => AddressFactory::new(),
+                'email' => $faker->unique()->safeEmail(),
+                'biography' => $faker->optional(0.9)->paragraph(),
+            ]);
+        } else {
+            $defaults = array_merge($defaults, [
+                'firstname' => null,
+                'lastname' => null,
+                'email' => null,
+                'biography' => null,
             ]);
         }
 
         return $defaults;
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-     */
     protected function initialize(): static
     {
         return $this
-            // ->afterInstantiate(function(User $user): void {})
+            ->afterInstantiate(function (User $user): void {
+                if ($user->getRoles() === ['GUEST']) {
+                    $user->setFirstname(null);
+                    $user->setLastname(null);
+                    $user->setEmail(null);
+                    $user->setBiography(null);
+                    $user->setAddress(null);
+                }
+            })
         ;
     }
 }
