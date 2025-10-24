644f07fe90453b6a3e3869cc7f9887e20626c70c
Nicolas Desban <nicolas.desban@etudiant.univ-reims.fr>
2025-10-24 15:19:43 +0200

    feat: Enhance CodeFixtures to dynamically create Hunts and linked TeamPlayers based on playability

--- src/DataFixtures/CodeFixtures.php
+++ src/DataFixtures/CodeFixtures.php
@@ -2,20 +2,45 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\CodeFactory;
 use App\Factory\HuntFactory;
 use App\Factory\TeamPlayerFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Random\RandomException;
 
 class CodeFixtures extends Fixture
 {
+    /**
+     * @throws RandomException
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        TeamPlayerFactory::createMany(20);
-        HuntFactory::createMany(20);
+        $hunts = HuntFactory::createMany(20, function () {
+            $isPlayable = 1 === random_int(0, 1);
+
+            return [
+                'isTeamPlayable' => $isPlayable,
+                'teamPlayerMax' => $isPlayable ? random_int(2, 8) : null,
+            ];
+        });
+
+        foreach ($hunts as $hunt) {
+            if (!$hunt->isTeamPlayable()) {
+                continue;
+            }
+
+            $max = $hunt->getTeamPlayerMax() ?? 5;
+            $count = random_int(1, min(5, max(1, $max)));
+
+            TeamPlayerFactory::createMany($count, function () use ($hunt) {
+                return [
+                    'hunt' => $hunt,
+                    'isPublic' => (bool) random_int(0, 1),
+                    'nbPlayers' => random_int(1, max(1, $hunt->getNbPlayers() ?? 4)),
+                ];
+            });
+        }
+
         $manager->flush();
     }
 }
