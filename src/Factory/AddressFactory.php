bf8c9efa57ace8f77ef04d10d008e0bd903babe1
Nicolas Desban <nicolas.desban@etudiant.univ-reims.fr>
2025-10-24 12:01:09 +0200

    feat: Enhance AddressFactory to support additional countries and improve address generation logic

--- src/Factory/AddressFactory.php
+++ src/Factory/AddressFactory.php
@@ -27,27 +27,55 @@ final class AddressFactory extends PersistentProxyObjectFactory
 
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
      */
     protected function defaults(): array|callable
     {
         $faker = Factory::create('fr_FR');
-        $country = $faker->randomElement(['France', 'United States', 'Germany', 'Italy', 'Spain']);
+
+        $countries = array_merge(array_fill(0, 75, 'France'), ['United States', 'Germany', 'Italy', 'Spain', 'Belgium', 'Switzerland']);
+        $country = $faker->randomElement($countries);
+
         $fakerByCountry = match ($country) {
             'France' => Factory::create('fr_FR'),
             'United States' => Factory::create('en_US'),
             'Germany' => Factory::create('de_DE'),
             'Italy' => Factory::create('it_IT'),
             'Spain' => Factory::create('es_ES'),
+            'Belgium' => Factory::create('fr_BE'),
+            'Switzerland' => Factory::create('de_CH'),
             default => Factory::create(),
         };
 
+        if ('France' === $country) {
+            $majorCities = ['Paris', 'Marseille', 'Lyon', 'Toulouse', 'Nice', 'Nantes', 'Strasbourg', 'Montpellier', 'Bordeaux', 'Lille', 'Reims'];
+            $city = $fakerByCountry->boolean(60) ? $fakerByCountry->randomElement($majorCities) : $fakerByCountry->city();
+        } else {
+            $city = $fakerByCountry->city();
+        }
+
+        $postCode = (string) $fakerByCountry->postcode();
+        if (mb_strlen($postCode) > 10) {
+            $postCode = mb_substr($postCode, 0, 10);
+        }
+
+        $street = $fakerByCountry->streetAddress();
+        if (mb_strlen($street) > 100) {
+            $street = mb_substr($street, 0, 100);
+        }
+
+        if (mb_strlen($city) > 50) {
+            $city = mb_substr($city, 0, 50);
+        }
+
+        if (mb_strlen($country) > 30) {
+            $country = mb_substr($country, 0, 30);
+        }
+
         return [
-            'city' => $fakerByCountry->city(),
             'country' => $country,
-            'postCode' => $fakerByCountry->postcode(),
-            'street' => $fakerByCountry->streetAddress(),
+            'city' => $city,
+            'postCode' => $postCode,
+            'street' => $street,
         ];
     }
 
