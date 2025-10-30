6802c7e6814fa3fdd7e3ab5f38a60cc177be2601
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-10-30 15:45:04 +0100

    build: Transform address entity into an API

--- src/Entity/Address.php
+++ src/Entity/Address.php
@@ -2,35 +2,66 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\AddressRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: AddressRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(security: "is_granted('ROLE_USER')"),
+        new Put(security: "is_granted('ROLE_USER')"),
+        new Patch(security: "is_granted('ROLE_USER')"),
+        new Delete(security: "is_granted('ROLE_ADMIN')"),
+    ],
+    normalizationContext: ['groups' => ['address:read']],
+    denormalizationContext: ['groups' => ['address:write']]
+)]
+#[ApiFilter(SearchFilter::class, properties: ['country' => 'partial', 'city' => 'partial', 'postCode' => 'exact'])]
+#[ApiFilter(OrderFilter::class, properties: ['country', 'city', 'postCode'], arguments: ['orderParameterName' => 'order'])]
 class Address
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['address:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 30)]
+    #[Groups(['address:read', 'address:write'])]
     private ?string $country = null;
 
     #[ORM\Column(length: 50)]
+    #[Groups(['address:read', 'address:write'])]
     private ?string $city = null;
 
     #[ORM\Column(length: 10)]
+    #[Groups(['address:read', 'address:write'])]
     private ?string $postCode = null;
 
     #[ORM\Column(length: 100)]
+    #[Groups(['address:read', 'address:write'])]
     private ?string $street = null;
 
     /**
      * @var Collection<int, Hunt>
      */
     #[ORM\OneToMany(targetEntity: Hunt::class, mappedBy: 'location')]
+    #[Groups(['address:read'])]
     private Collection $hunts;
 
     /**
