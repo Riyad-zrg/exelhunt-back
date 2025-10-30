935fa18c82dbbe714af1bf0995c6e6ff95c7d888
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-10-30 15:45:56 +0100

    build: Transform Participation entity into an API

--- src/Entity/Participation.php
+++ src/Entity/Participation.php
@@ -2,34 +2,65 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\ParticipationRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ParticipationRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(security: "is_granted('ROLE_USER')"),
+        new Put(security: "is_granted('ROLE_USER')"),
+        new Patch(security: "is_granted('ROLE_USER')"),
+        new Delete(security: "is_granted('ROLE_ADMIN')"),
+    ],
+    normalizationContext: ['groups' => ['participation:read']],
+    denormalizationContext: ['groups' => ['participation:write']]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'hunt.id' => 'exact',
+    'player.id' => 'exact',
+    'tracking' => 'partial',
+])]
+#[ApiFilter(OrderFilter::class, properties: ['globalTime', 'tracking'], arguments: ['orderParameterName' => 'order'])]
 class Participation
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['participation:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 15)]
+    #[Groups(['participation:read', 'participation:write'])]
     private ?string $tracking = null;
 
     #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    #[Groups(['participation:read', 'participation:write'])]
     private ?\DateTime $globalTime = null;
 
     #[ORM\ManyToOne(inversedBy: 'participations')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?Hunt $hunt = null;
+    #[Groups(['participation:read', 'participation:write'])]
+    private ?Hunt $Hunt = null;
 
     #[ORM\ManyToOne(inversedBy: 'participations')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?User $player = null;
-
-    #[ORM\ManyToOne(inversedBy: 'participation')]
-    private ?TeamPlayer $teamPlayer = null;
+    #[Groups(['participation:read', 'participation:write'])]
+    private ?User $Player = null;
 
     public function getId(): ?int
     {
@@ -62,36 +93,24 @@ class Participation
 
     public function getHunt(): ?Hunt
     {
-        return $this->hunt;
+        return $this->Hunt;
     }
 
-    public function setHunt(?Hunt $hunt): static
+    public function setHunt(?Hunt $Hunt): static
     {
-        $this->hunt = $hunt;
+        $this->Hunt = $Hunt;
 
         return $this;
     }
 
     public function getPlayer(): ?User
     {
-        return $this->player;
+        return $this->Player;
     }
 
-    public function setPlayer(?User $player): static
+    public function setPlayer(?User $Player): static
     {
-        $this->player = $player;
-
-        return $this;
-    }
-
-    public function getTeamPlayer(): ?TeamPlayer
-    {
-        return $this->teamPlayer;
-    }
-
-    public function setTeamPlayer(?TeamPlayer $teamPlayer): static
-    {
-        $this->teamPlayer = $teamPlayer;
+        $this->Player = $Player;
 
         return $this;
     }
