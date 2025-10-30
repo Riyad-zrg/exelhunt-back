05f5132a746d904a82b560daadbc44eb8f8da6d7
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-10-30 15:45:12 +0100

    build: Transform Code entity into an API

--- src/Entity/Code.php
+++ src/Entity/Code.php
@@ -2,48 +2,71 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\CodeRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CodeRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(security: "is_granted('ROLE_USER')"),
+        new Put(security: "is_granted('ROLE_USER')"),
+        new Patch(security: "is_granted('ROLE_USER')"),
+        new Delete(security: "is_granted('ROLE_ADMIN')"),
+    ],
+    normalizationContext: ['groups' => ['code:read']],
+    denormalizationContext: ['groups' => ['code:write']]
+)]
+#[ApiFilter(SearchFilter::class, properties: ['code' => 'exact', 'hunt.id' => 'exact'])]
+#[ApiFilter(OrderFilter::class, properties: ['createdAt', 'expireAt', 'code'], arguments: ['orderParameterName' => 'order'])]
 class Code
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['code:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 6, unique: true)]
-    private ?string $code = null;
+    #[Groups(['code:read', 'code:write'])]
+    private ?int $code = null;
 
-    #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
-    #[Gedmo\Timestampable]
+    #[ORM\Column]
+    #[Groups(['code:read', 'code:write'])]
     private ?\DateTimeImmutable $createdAt = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    #[Gedmo\Timestampable]
-    private ?\DateTime $expireAt = null;
+    #[ORM\Column]
+    #[Groups(['code:read', 'code:write'])]
+    private ?\DateTimeImmutable $expireAt = null;
 
     #[ORM\OneToOne(inversedBy: 'code')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?Hunt $hunt = null;
-
-    #[ORM\OneToOne(mappedBy: 'code', cascade: ['persist', 'remove'])]
-    private ?TeamPlayer $teamPlayer = null;
+    #[Groups(['code:read', 'code:write'])]
+    private ?Hunt $Hunt = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getCode(): ?string
+    public function getCode(): ?int
     {
         return $this->code;
     }
 
-    public function setCode(string $code): static
+    public function setCode(int $code): static
     {
         $this->code = $code;
 
@@ -62,12 +85,12 @@ class Code
         return $this;
     }
 
-    public function getExpireAt(): ?\DateTime
+    public function getExpireAt(): ?\DateTimeImmutable
     {
         return $this->expireAt;
     }
 
-    public function setExpireAt(\DateTime $expireAt): static
+    public function setExpireAt(\DateTimeImmutable $expireAt): static
     {
         $this->expireAt = $expireAt;
 
@@ -76,34 +99,12 @@ class Code
 
     public function getHunt(): ?Hunt
     {
-        return $this->hunt;
+        return $this->Hunt;
     }
 
-    public function setHunt(?Hunt $hunt): static
+    public function setHunt(?Hunt $Hunt): static
     {
-        $this->hunt = $hunt;
-
-        return $this;
-    }
-
-    public function getTeamPlayer(): ?TeamPlayer
-    {
-        return $this->teamPlayer;
-    }
-
-    public function setTeamPlayer(?TeamPlayer $teamPlayer): static
-    {
-        // unset the owning side of the relation if necessary
-        if (null === $teamPlayer && null !== $this->teamPlayer) {
-            $this->teamPlayer->setCode(null);
-        }
-
-        // set the owning side of the relation if necessary
-        if (null !== $teamPlayer && $teamPlayer->getCode() !== $this) {
-            $teamPlayer->setCode($this);
-        }
-
-        $this->teamPlayer = $teamPlayer;
+        $this->Hunt = $Hunt;
 
         return $this;
     }
