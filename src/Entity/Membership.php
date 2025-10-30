bc081543b960d66f2c3cd79f9a5b7b0201a2a1ba
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-10-30 15:45:42 +0100

    build: Transform Membership entity into an API

--- src/Entity/Membership.php
+++ src/Entity/Membership.php
@@ -2,32 +2,65 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\MembershipRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: MembershipRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(security: "is_granted('ROLE_USER')"),
+        new Put(security: "is_granted('ROLE_USER')"),
+        new Patch(security: "is_granted('ROLE_USER')"),
+        new Delete(security: "is_granted('ROLE_ADMIN')"),
+    ],
+    normalizationContext: ['groups' => ['membership:read']],
+    denormalizationContext: ['groups' => ['membership:write']]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'member.id' => 'exact',
+    'team.id' => 'exact',
+    'role' => 'partial',
+])]
+#[ApiFilter(DateFilter::class, properties: ['joinedAt'])]
+#[ApiFilter(OrderFilter::class, properties: ['joinedAt'], arguments: ['orderParameterName' => 'order'])]
 class Membership
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['membership:read'])]
     private ?int $id = null;
 
-    #[ORM\Column(type: Types::JSON)]
+    #[ORM\Column(length: 50)]
+    #[Groups(['membership:read', 'membership:write'])]
     private array $role = [];
-    #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
-    #[Gedmo\Timestampable]
+    #[ORM\Column]
+    #[Groups(['membership:read', 'membership:write'])]
     private ?\DateTimeImmutable $joinedAt = null;
 
     #[ORM\ManyToOne(inversedBy: 'memberships')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?User $member = null;
+    #[Groups(['membership:read', 'membership:write'])]
+    private ?User $Member = null;
 
     #[ORM\ManyToOne(inversedBy: 'memberships')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?Team $team = null;
+    #[Groups(['membership:read', 'membership:write'])]
+    private ?Team $Team = null;
 
     public function getId(): ?int
     {
@@ -48,24 +81,24 @@ class Membership
 
     public function getMember(): ?User
     {
-        return $this->member;
+        return $this->Member;
     }
 
-    public function setMember(?User $member): static
+    public function setMember(?User $Member): static
     {
-        $this->member = $member;
+        $this->Member = $Member;
 
         return $this;
     }
 
     public function getTeam(): ?Team
     {
-        return $this->team;
+        return $this->Team;
     }
 
-    public function setTeam(?Team $team): static
+    public function setTeam(?Team $Team): static
     {
-        $this->team = $team;
+        $this->Team = $Team;
 
         return $this;
     }
