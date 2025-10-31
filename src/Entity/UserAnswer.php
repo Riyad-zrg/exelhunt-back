5ad50c204f2989f33f3a66db5cbdcd08430e4b8a
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-10-31 12:27:25 +0100

    build: Transform UserAnswer entity into an API

--- src/Entity/UserAnswer.php
+++ src/Entity/UserAnswer.php
@@ -2,27 +2,58 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\UserAnswerRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserAnswerRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(security: "is_granted('ROLE_USER')"),
+        new Put(security: "is_granted('ROLE_USER')"),
+        new Patch(security: "is_granted('ROLE_USER')"),
+        new Delete(security: "is_granted('ROLE_ADMIN')"),
+    ],
+    normalizationContext: ['groups' => ['user_answer:read']],
+    denormalizationContext: ['groups' => ['user_answer:write']]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'puzzleAnswer.id' => 'exact',
+    'isCorrect' => 'exact',
+])]
+#[ApiFilter(OrderFilter::class, properties: ['sendAt', 'id'], arguments: ['orderParameterName' => 'order'])]
 class UserAnswer
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['user_answer:read'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::JSON)]
+    #[Groups(['user_answer:read', 'user_answer:write'])]
     private array $answerContent = [];
 
     #[ORM\Column]
+    #[Groups(['user_answer:read', 'user_answer:write'])]
     private ?bool $isCorrect = null;
 
     #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
     #[Gedmo\Timestampable]
+    #[Groups(['user_answer:read', 'user_answer:write'])]
     private ?\DateTimeImmutable $sendAt = null;
 
     #[ORM\ManyToOne(inversedBy: 'userAnswers')]
@@ -31,6 +62,7 @@ class UserAnswer
 
     #[ORM\ManyToOne(inversedBy: 'userAnswers')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['user_answer:read', 'user_answer:write'])]
     private ?Puzzle $puzzle = null;
 
     public function getId(): ?int
