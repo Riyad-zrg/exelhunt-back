1ca974ab53ba9ae2e5d6823e1db7f988ea1a1a67
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-10-31 12:27:13 +0100

    build: Transform User entity into an API

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,66 +2,113 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 class User implements \Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface
+#[ApiResource(
+    operations: [
+        new Get(security: "is_granted('ROLE_ADMIN') or object == user"),
+        new GetCollection(security: "is_granted('ROLE_ADMIN')"),
+        new Post(security: "is_granted('PUBLIC_ACCESS')"),
+        new Put(security: "is_granted('ROLE_ADMIN') or object == user"),
+        new Patch(security: "is_granted('ROLE_ADMIN') or object == user"),
+        new Delete(security: "is_granted('ROLE_ADMIN') or object == user"),
+    ],
+    normalizationContext: ['groups' => ['user:read']],
+    denormalizationContext: ['groups' => ['user:write']]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'nickname' => 'partial',
+    'email' => 'partial',
+    'address.city' => 'partial',
+    'address.country' => 'partial',
+])]
+#[ApiFilter(OrderFilter::class, properties: ['createdAt', 'nickname'], arguments: ['orderParameterName' => 'order'])]
+class User
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['user:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 30)]
+    #[Groups(['user:read', 'user:write'])]
     private ?string $nickname = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['user:write'])]
     private ?string $password = null;
 
     #[ORM\Column(type: Types::JSON)]
+    #[Groups(['user:read', 'user:write'])]
     private array $roles = [];
 
     #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
     #[Gedmo\Timestampable]
+    #[Groups(['user:read', 'user:write'])]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['user:read', 'user:write'])]
     private ?string $avatar = null;
 
     #[ORM\Column(length: 30, nullable: true)]
+    #[Groups(['user:read', 'user:write'])]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 30, nullable: true)]
+    #[Groups(['user:read', 'user:write'])]
     private ?string $lastname = null;
 
     #[ORM\Column(length: 100, nullable: true)]
+    #[Groups(['user:read', 'user:write'])]
     private ?string $email = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Groups(['user:read', 'user:write'])]
     private ?string $biography = null;
+
+    #[ORM\ManyToOne]
+    #[Groups(['user:read', 'user:write'])]
+    private ?Address $Address = null;
 
     /**
      * @var Collection<int, Membership>
      */
     #[ORM\OneToMany(targetEntity: Membership::class, mappedBy: 'member', orphanRemoval: true)]
+    #[Groups(['user:read'])]
     private Collection $memberships;
 
     /**
      * @var Collection<int, Participation>
      */
     #[ORM\OneToMany(targetEntity: Participation::class, mappedBy: 'player')]
+    #[Groups(['user:read'])]
     private Collection $participations;
 
     /**
      * @var Collection<int, Start>
      */
     #[ORM\OneToMany(targetEntity: Start::class, mappedBy: 'player')]
+    #[Groups(['user:read'])]
     private Collection $startPuzzle;
 
     /**
@@ -190,6 +237,18 @@ class User implements \Symfony\Component\Security\Core\User\PasswordAuthenticate
     public function setBiography(?string $biography): static
     {
         $this->biography = $biography;
+
+        return $this;
+    }
+
+    public function getAddress(): ?Address
+    {
+        return $this->Address;
+    }
+
+    public function setAddress(?Address $Address): static
+    {
+        $this->Address = $Address;
 
         return $this;
     }
