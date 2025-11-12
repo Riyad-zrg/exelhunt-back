845a60ce29acfcf290986c2aaf66215e49e5026a
Nicolas DESBAN <nicolas.desban@etudiant.univ-reims.fr>
2025-11-12 01:06:59 +0100

    feat: Enhance UserAnswerFactory to improve content generation and answer validation logic

--- src/Factory/UserAnswerFactory.php
+++ src/Factory/UserAnswerFactory.php
@@ -3,6 +3,7 @@
 namespace App\Factory;
 
 use App\Entity\UserAnswer;
+use Random\RandomException;
 use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
 
 /**
@@ -10,70 +11,115 @@ use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
  */
 final class UserAnswerFactory extends PersistentProxyObjectFactory
 {
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
-     */
-    public function __construct()
-    {
-    }
-
     public static function class(): string
     {
         return UserAnswer::class;
     }
 
     /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
+     * @throws RandomException
      */
     protected function defaults(): array|callable
     {
         $faker = self::faker();
-        $puzzleAnswer = PuzzleAnswerFactory::random();
+
+        $puzzle = PuzzleFactory::randomOrCreate();
+        $type = $puzzle->getTypeAnswer();
+        $puzzleContent = $puzzle->getContentAnswerJSON();
+
+        $player = UserFactory::randomOrCreate();
 
         $content = [];
-        $isCorrect = $faker->boolean();
+        $isCorrect = false;
 
-        if ($puzzleAnswer) {
-            switch ($puzzleAnswer->getType()) {
-                case 'qcm':
-                    $options = $puzzleAnswer->getContent()['options'] ?? [];
-                    $selected = $faker->randomElement($options);
-                    $content = ['selected' => $selected['label']];
-                    $isCorrect = $selected['isCorrect'];
-                    break;
+        switch (strtoupper($type)) {
+            case 'QCM':
+                $options = $puzzleContent['options'] ?? [];
+                $numOptions = count($options);
+                if (0 === $numOptions) {
+                    $content = ['selected' => []];
+                } else {
+                    $multiple = $puzzleContent['multiple'] ?? false;
+                    if ($multiple) {
+                        $selectedCount = $faker->numberBetween(1, max(1, min(3, $numOptions)));
+                        $selected = $faker->randomElements(range(0, $numOptions - 1), $selectedCount);
+                    } else {
+                        $selected = [$faker->numberBetween(0, $numOptions - 1)];
+                    }
+                    $content = ['selected' => $selected];
 
-                case 'text':
-                    $content = ['answer' => $faker->sentence(3)];
-                    break;
+                    $correct = $puzzleContent['correct'] ?? [];
+                    $isCorrect = !empty(array_intersect($selected, $correct)) && empty(array_diff($selected, $correct));
+                }
+                break;
 
-                case 'qrcode':
-                    $content = ['code' => $faker->uuid()];
-                    break;
+            case 'QRO':
+            case 'QRO ':
+                $accepted = $puzzleContent['acceptedAnswers'] ?? [];
+                if (!empty($accepted) && $faker->boolean(70)) {
+                    $answer = $faker->randomElement($accepted);
+                    $content = ['answer' => $answer];
+                    $isCorrect = true;
+                } else {
+                    $answer = $faker->sentence(3);
+                    $content = ['answer' => $answer];
+                }
+                break;
 
-                case 'gps':
+            case 'QR':
+            case 'QRCODE':
+                $code = $puzzleContent['code'] ?? $faker->bothify('QR-??-#####');
+                if ($faker->boolean(85)) {
+                    $content = ['code' => $code];
+                    $isCorrect = true;
+                } else {
+                    $content = ['code' => 'WRONG-'.$faker->bothify('??-#####')];
+                }
+                break;
+
+            case 'GPS':
+                $lat = $puzzleContent['lat'] ?? $faker->latitude(43, 50);
+                $lng = $puzzleContent['lng'] ?? $faker->longitude(-1, 7);
+                $radius = $puzzleContent['radius'] ?? 50;
+
+                if ($faker->boolean(80)) {
+                    $delta = $radius / 111000.0; // approx degrees
                     $content = [
-                        'latitude' => $faker->latitude(),
-                        'longitude' => $faker->longitude(),
+                        'latitude' => $lat + $faker->randomFloat(6, -$delta, $delta),
+                        'longitude' => $lng + $faker->randomFloat(6, -$delta, $delta),
                     ];
-                    break;
-            }
+                    $isCorrect = true;
+                } else {
+                    $content = [
+                        'latitude' => $lat + $faker->numberBetween(1, 10),
+                        'longitude' => $lng + $faker->numberBetween(1, 10),
+                    ];
+                }
+                break;
+
+            default:
+                $content = ['answer' => $faker->sentence(3)];
+                $isCorrect = (bool) $faker->boolean(50);
+                break;
         }
 
+        $earliest = new \DateTimeImmutable('-30 days');
+        if ($puzzle->getHunt() && $puzzle->getHunt()->getCreatedAt() instanceof \DateTimeInterface) {
+            $earliest = \DateTimeImmutable::createFromMutable(\DateTime::createFromFormat('Y-m-d H:i:s', $puzzle->getHunt()->getCreatedAt()->format('Y-m-d H:i:s')));
+        }
+
+        $sendAtTs = random_int($earliest->getTimestamp(), (new \DateTimeImmutable('now'))->getTimestamp());
+        $sendAt = (new \DateTimeImmutable())->setTimestamp($sendAtTs);
+
         return [
-            'content' => $content,
+            'contentAnswerJSON' => $content,
             'isCorrect' => $isCorrect,
-            'sendAt' => \DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-10 days', 'now')),
-            'puzzleAnswer' => $puzzleAnswer,
+            'sendAt' => $sendAt,
+            'player' => $player,
+            'puzzle' => $puzzle,
         ];
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-     */
     protected function initialize(): static
     {
         return $this
