c783ff795a3d15957fc2d6d229bcaabbe433fa69
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-11-17 11:25:20 +0100

    style: Add address-row class for improved spacing in address form fields

--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -13,10 +13,10 @@ class AddressType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('country', TextType::class, ['required' => false, 'label' => 'Pays', 'attr' => ['placeholder' => 'France']])
-            ->add('city', TextType::class, ['required' => false, 'label' => 'Ville', 'attr' => ['placeholder' => 'Paris']])
-            ->add('postCode', TextType::class, ['required' => false, 'label' => 'Code Postal', 'attr' => ['placeholder' => '75000']])
-            ->add('street', TextType::class, ['required' => false, 'label' => 'Rue', 'attr' => ['placeholder' => '10 rue de la Paix']]);
+            ->add('country', TextType::class, ['required' => false, 'label' => 'Pays', 'attr' => ['placeholder' => 'France'], 'row_attr' => ['class' => 'address-row']])
+            ->add('city', TextType::class, ['required' => false, 'label' => 'Ville', 'attr' => ['placeholder' => 'Paris'], 'row_attr' => ['class' => 'address-row']])
+            ->add('postCode', TextType::class, ['required' => false, 'label' => 'Code Postal', 'attr' => ['placeholder' => '75000'], 'row_attr' => ['class' => 'address-row']])
+            ->add('street', TextType::class, ['required' => false, 'label' => 'Rue', 'attr' => ['placeholder' => '10 rue de la Paix'], 'row_attr' => ['class' => 'address-row']]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
