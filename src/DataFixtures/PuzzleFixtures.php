a14da68a16f56f9732c259afe93f77e58a1f7954
Nicolas DESBAN <nicolas.desban@etudiant.univ-reims.fr>
2025-11-17 03:47:24 +0100

    feat: Update PuzzleFixtures to implement DependentFixtureInterface and define dependencies

--- src/DataFixtures/PuzzleFixtures.php
+++ src/DataFixtures/PuzzleFixtures.php
@@ -3,12 +3,20 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class PuzzleFixtures extends Fixture
+class PuzzleFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            HuntFixtures::class,
+        ];
     }
 }
