919ecccf1d66183755ce0f3dd652d71af225d58f
Nicolas DESBAN <nicolas.desban@etudiant.univ-reims.fr>
2025-11-17 03:47:32 +0100

    feat: Update TeamCreatorFixtures to reduce the number of created entities from 100 to 20

--- src/DataFixtures/TeamCreatorFixtures.php
+++ src/DataFixtures/TeamCreatorFixtures.php
@@ -5,13 +5,12 @@ namespace App\DataFixtures;
 use App\Factory\TeamCreatorFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Random\RandomException;
 
 class TeamCreatorFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        TeamCreatorFactory::createMany(100);
+        TeamCreatorFactory::createMany(20);
         $manager->flush();
     }
 }
