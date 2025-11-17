b1a14b369b6c7035f4b171750772d684d2cceaf7
Nicolas DESBAN <nicolas.desban@etudiant.univ-reims.fr>
2025-11-17 03:46:46 +0100

    feat: Enhance UserAnswerFactory to improve answer content generation and correctness evaluation

--- src/Factory/UserAnswerFactory.php
+++ src/Factory/UserAnswerFactory.php
@@ -24,96 +24,18 @@ final class UserAnswerFactory extends PersistentProxyObjectFactory
         $faker = self::faker();
 
         $puzzle = PuzzleFactory::randomOrCreate();
-        $type = $puzzle->getTypeAnswer();
-        $puzzleContent = $puzzle->getContentAnswerJSON();
-
         $player = UserFactory::randomOrCreate();
 
         $content = [];
         $isCorrect = false;
 
-        switch (strtoupper($type)) {
-            case 'QCM':
-                $options = $puzzleContent['options'] ?? [];
-                $numOptions = count($options);
-                if (0 === $numOptions) {
-                    $content = ['selected' => []];
-                } else {
-                    $multiple = $puzzleContent['multiple'] ?? false;
-                    if ($multiple) {
-                        $selectedCount = $faker->numberBetween(1, max(1, min(3, $numOptions)));
-                        $selected = $faker->randomElements(range(0, $numOptions - 1), $selectedCount);
-                    } else {
-                        $selected = [$faker->numberBetween(0, $numOptions - 1)];
-                    }
-                    $content = ['selected' => $selected];
-
-                    $correct = $puzzleContent['correct'] ?? [];
-                    $isCorrect = !empty(array_intersect($selected, $correct)) && empty(array_diff($selected, $correct));
-                }
-                break;
-
-            case 'QRO':
-            case 'QRO ':
-                $accepted = $puzzleContent['acceptedAnswers'] ?? [];
-                if (!empty($accepted) && $faker->boolean(70)) {
-                    $answer = $faker->randomElement($accepted);
-                    $content = ['answer' => $answer];
-                    $isCorrect = true;
-                } else {
-                    $answer = $faker->sentence(3);
-                    $content = ['answer' => $answer];
-                }
-                break;
-
-            case 'QR':
-            case 'QRCODE':
-                $code = $puzzleContent['code'] ?? $faker->bothify('QR-??-#####');
-                if ($faker->boolean(85)) {
-                    $content = ['code' => $code];
-                    $isCorrect = true;
-                } else {
-                    $content = ['code' => 'WRONG-'.$faker->bothify('??-#####')];
-                }
-                break;
-
-            case 'GPS':
-                $lat = $puzzleContent['lat'] ?? $faker->latitude(43, 50);
-                $lng = $puzzleContent['lng'] ?? $faker->longitude(-1, 7);
-                $radius = $puzzleContent['radius'] ?? 50;
-
-                if ($faker->boolean(80)) {
-                    $delta = $radius / 111000.0; // approx degrees
-                    $content = [
-                        'latitude' => $lat + $faker->randomFloat(6, -$delta, $delta),
-                        'longitude' => $lng + $faker->randomFloat(6, -$delta, $delta),
-                    ];
-                    $isCorrect = true;
-                } else {
-                    $content = [
-                        'latitude' => $lat + $faker->numberBetween(1, 10),
-                        'longitude' => $lng + $faker->numberBetween(1, 10),
-                    ];
-                }
-                break;
-
-            default:
-                $content = ['answer' => $faker->sentence(3)];
-                $isCorrect = (bool) $faker->boolean(50);
-                break;
-        }
-
         $earliest = new \DateTimeImmutable('-30 days');
-        if ($puzzle->getHunt() && $puzzle->getHunt()->getCreatedAt() instanceof \DateTimeInterface) {
-            $earliest = \DateTimeImmutable::createFromMutable(\DateTime::createFromFormat('Y-m-d H:i:s', $puzzle->getHunt()->getCreatedAt()->format('Y-m-d H:i:s')));
-        }
-
         $sendAtTs = random_int($earliest->getTimestamp(), (new \DateTimeImmutable('now'))->getTimestamp());
         $sendAt = (new \DateTimeImmutable())->setTimestamp($sendAtTs);
 
         return [
-            'contentAnswerJSON' => $content,
-            'isCorrect' => $isCorrect,
+            'answerContent' => $content,
+            'isCorrect' => false,
             'sendAt' => $sendAt,
             'player' => $player,
             'puzzle' => $puzzle,
@@ -123,7 +45,97 @@ final class UserAnswerFactory extends PersistentProxyObjectFactory
     protected function initialize(): static
     {
         return $this
-            // ->afterInstantiate(function(UserAnswer $userAnswer): void {})
+            ->afterInstantiate(function (UserAnswer $userAnswer): void {
+                $faker = self::faker();
+                $puzzle = $userAnswer->getPuzzle();
+
+                if (!$puzzle) {
+                    return;
+                }
+
+                $type = trim($puzzle->getTypeAnswer());
+                $puzzleContent = $puzzle->getAnswerContent();
+
+                $isCorrect = false;
+
+                switch (strtoupper($type)) {
+                    case 'QCM':
+                        $options = $puzzleContent['options'] ?? [];
+                        $numOptions = count($options);
+                        if (0 === $numOptions) {
+                            $content = ['selected' => []];
+                        } else {
+                            $multiple = $puzzleContent['multiple'] ?? false;
+                            $correct = $puzzleContent['correct'] ?? [];
+
+                            if ($faker->boolean(70)) {
+                                $selected = $correct;
+                                $isCorrect = true;
+                            } else {
+                                if ($multiple) {
+                                    $selectedCount = $faker->numberBetween(1, max(1, min(3, $numOptions)));
+                                    $selected = $faker->randomElements(range(0, $numOptions - 1), $selectedCount);
+                                } else {
+                                    $selected = [$faker->numberBetween(0, $numOptions - 1)];
+                                }
+                                $isCorrect = !empty(array_intersect($selected, $correct)) && empty(array_diff($selected, $correct));
+                            }
+                            $content = ['selected' => $selected];
+                        }
+                        break;
+
+                    case 'QRO':
+                        $accepted = $puzzleContent['acceptedAnswers'] ?? [];
+                        if (!empty($accepted) && $faker->boolean(70)) {
+                            $answer = $faker->randomElement($accepted);
+                            $content = ['answer' => $answer];
+                            $isCorrect = true;
+                        } else {
+                            $answer = $faker->sentence(3);
+                            $content = ['answer' => $answer];
+                        }
+                        break;
+
+                    case 'QR':
+                        $expectedCode = $puzzleContent['code'] ?? 'QR-CODE-'.strtoupper($faker->bothify('??##??##'));
+                        if ($faker->boolean(70)) {
+                            $content = ['code' => $expectedCode];
+                            $isCorrect = true;
+                        } else {
+                            $content = ['code' => 'QR-CODE-'.strtoupper($faker->bothify('??##??##'))];
+                        }
+                        break;
+
+                    case 'GPS':
+                        $expectedLat = $puzzleContent['lat'] ?? $faker->latitude();
+                        $expectedLng = $puzzleContent['lng'] ?? $faker->longitude();
+                        $radius = $puzzleContent['radius'] ?? 50;
+
+                        if ($faker->boolean(70)) {
+                            $offsetLat = $faker->randomFloat(6, -0.0001, 0.0001) * ($radius / 50);
+                            $offsetLng = $faker->randomFloat(6, -0.0001, 0.0001) * ($radius / 50);
+                            $content = [
+                                'lat' => $expectedLat + $offsetLat,
+                                'lng' => $expectedLng + $offsetLng,
+                            ];
+                            $isCorrect = true;
+                        } else {
+                            $content = [
+                                'lat' => $faker->latitude(),
+                                'lng' => $faker->longitude(),
+                            ];
+                        }
+                        break;
+
+                    default:
+                        $content = ['answer' => $faker->sentence(3)];
+                        $isCorrect = $faker->boolean(30);
+                        break;
+                }
+
+                $userAnswer->setAnswerContent($content);
+                $userAnswer->setIsCorrect($isCorrect);
+            })
         ;
     }
 }
