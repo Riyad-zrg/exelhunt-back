118ff242b0d8bb2bfc253400484e0fb84787fc85
lebe0069 <killyan.lebegue@etudiant.univ-reims.fr>
2025-11-25 12:02:37 +0100

    feat: add media field to Puzzle entity and update serialization groups

---
+++ migrations/Version20251125103345.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251125103345 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE puzzle ADD media TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE team ALTER avatar TYPE TEXT');
+        $this->addSql('ALTER TABLE team ALTER avatar TYPE TEXT');
+        $this->addSql('ALTER TABLE "user" DROP reset_token');
+        $this->addSql('ALTER TABLE "user" DROP reset_token_expires_at');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE puzzle DROP media');
+        $this->addSql('ALTER TABLE team ALTER avatar TYPE VARCHAR(5000)');
+        $this->addSql('ALTER TABLE "user" ADD reset_token VARCHAR(100) DEFAULT NULL');
+        $this->addSql('ALTER TABLE "user" ADD reset_token_expires_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+        $this->addSql('COMMENT ON COLUMN "user".reset_token_expires_at IS \'(DC2Type:datetime_immutable)\'');
+    }
+}
